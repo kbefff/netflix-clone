319e9504e916202e219b6b0ad12055e8ed41e853
Kelsey Beffel <kelseybeffel@Kelseys-Laptop.local>
2023-09-13 20:17:05 -0700

    stup tailwind and hero image on new auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,11 @@
+const Auth = () => {
+
+    return (
+        // <div className="h-screen bg-[url('../public/images/hero.jpg')] bg-no-repeat>
+        // <div className="relative h-full w-full bg-[url('../public/images/hero.jpg')] bg-no-repeat bg-center">
+        <div className="h-screen bg-[url('../public/images/hero.jpg')] bg-no-repeat bg-center">
+
+        </div>
+    )
+}
+export default Auth;
